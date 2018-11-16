a62c00bad12ca152d999da6fff316217f073891f
“Foenix” <lzy1998@mail.ustc.edu.cn>
2018-11-16 12:17:46 +0800

    test3

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from config import config
 from pynput.keyboard import Listener
 
 def press(key):
-	# x = subprocess.check_output(['gnome-screensaver-command','-q'])
-	# if("The screensaver is inactive" in str(x)):
-	# 	os.system("gnome-screensaver-command -l")
+	x = subprocess.check_output(['gnome-screensaver-command','-q'])
+	if("The screensaver is inactive" in str(x)):
+		os.system("gnome-screensaver-command -l")
 	print("-----------")
 	if("Key.esc" in str(key)):
 		return False
@@ -24,18 +24,18 @@ if __name__ == '__main__':
 	flag = 0
 	lll_flag = 0
 	while True:
-		# time.sleep(5)
-		# lzy = subprocess.check_output(['gnome-screensaver-command','-q'])
-		# print(str(lzy))
-		# if("The screensaver is inactive" in str(lzy)):#unlock
-		if lll_flag == 0:
+		# time.sleep(2)
+		lzy = subprocess.check_output(['gnome-screensaver-command','-q'])
+		print(str(lzy))
+		if("The screensaver is inactive" in str(lzy)):#unlock
+		# if lll_flag == 0:
 			print("+++++++++")
 			identify.TakePhoto("unknow.jpg")
-			if(identify.numberof("unknow.jpg") <= 0):
-				os.system("gnome-screensaver-command -l")
-				print(1)
-				lll_flag = 1
-				continue
+			# if(identify.numberof("unknow.jpg") <= 0):
+			# 	os.system("gnome-screensaver-command -l")
+			# 	print(1)
+			# 	lll_flag = 1
+			# 	continue
 			x = identify.COMPARE("owner.jpg", "unknow.jpg")
 			if x == 0:
 				flag = 0
@@ -53,14 +53,18 @@ if __name__ == '__main__':
 			with Listener(on_press = press) as listener:					
 				listener.join()
 			identify.TakePhoto("unknow.jpg")
-			if(identify.numberof("unknow.jpg") <= 0):
-				continue
+			# if(identify.numberof("unknow.jpg") <= 0):
+			# 	continue
 			x = identify.COMPARE("owner.jpg", "unknow.jpg")
 			if x == 0:
 				lll_flag = 0
 				l = locker()
 				l.unlock_workstation()
+				time.sleep(1)
 			else:
+				os.system("notify-send 'Wrong!'")
+				os.system("gnome-screensaver-command -l")
+				time.sleep(1)
 				flag = flag + 1
 				if flag > 5:
 					Work_Encrypt("/home/foenix/test")
