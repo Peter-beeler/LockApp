72a0a93b7d1ca9912fa1d6227fd0348fe49f2ab5
“Foenix” <lzy1998@mail.ustc.edu.cn>
2018-11-16 15:26:24 +0800

    test5

--- main.py
+++ main.py
@@ -14,49 +14,49 @@ from mail import email
 from config import config
 from pynput.keyboard import Listener
 
+flag = 0
+tmp_flag = 0
+
 def press(key):
-	x = subprocess.check_output(['gnome-screensaver-command','-q'])
-	if("The screensaver is inactive" in str(x)):
-		os.system("gnome-screensaver-command -l")
-	# print("-----------")
+	global flag, tmp_flag
+	tmp_flag = tmp_flag + 1
 	if("Key.esc" in str(key)):
+		tmp_flag = 0
+		return False
+	if(("Key.enter" in str(key)) and tmp_flag > 10):
+		flag = flag + 1
+		time.sleep(0.5)
+		os.system("gnome-screensaver-command -l")
 		return False
 
 if __name__ == '__main__':
-	flag = 0
-	lll_flag = 0
 	while True:
 		lzy = subprocess.check_output(['gnome-screensaver-command','-q'])
 		print(str(lzy))
 		if("The screensaver is inactive" in str(lzy)):#unlock
-			# print("+++++++++")
 			identify.TakePhoto("unknow.jpg")
 			x = identify.COMPARE("owner.jpg", "unknow.jpg")
 			if x == 0:
-				flag = 0
-				time.sleep(1)
+				time.sleep(180)
 				continue
 			else:
-				lll_flag = 1
 				os.system("gnome-screensaver-command -l")
-				# print(2)
 		else:#lock
-			# print("***********")
 			with Listener(on_press = press) as listener:					
 				listener.join()
 			identify.TakePhoto("unknow.jpg")
 			x = identify.COMPARE("owner.jpg", "unknow.jpg")
 			if x == 0:
-				lll_flag = 0
 				l = locker()
 				l.unlock_workstation()
+				flag = 0
 				time.sleep(1)
 			else:
 				os.system("notify-send 'Wrong!'")
 				os.system("gnome-screensaver-command -l")
 				time.sleep(1)
 				flag = flag + 1
-				if flag > 5:
+				if flag > 4:
 					Work_Encrypt("/home/foenix/test")
 					Email = email("./mailViaPython/account.txt")
 					Email.sendMail()
