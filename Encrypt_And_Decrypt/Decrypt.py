2f97f8d4db02fbfa52d7420615ac4c0e4960dd0a
Peter <429744889@qq.com>
2018-11-17 17:12:52 +0800

    modify the location of the key

--- Encrypt_And_Decrypt/Decrypt.py
+++ Encrypt_And_Decrypt/Decrypt.py
@@ -1,28 +1,32 @@
 #coding=utf-8
-from Crypto.PublicKey import RSA
-from Crypto.Random import get_random_bytes
-from Crypto.Cipher import AES, PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Cipher import AES, PKCS1_OAEP
 import os
 import sys
-def Decrypt(filename):
-    code = 'nooneknows'
-    with open(filename, 'rb') as fobj:
-        private_key = RSA.import_key(open('my_private_rsa_key.bin').read(), passphrase=code)
-        enc_session_key, nonce, tag, ciphertext = [ fobj.read(x) 
-                                                    for x in (private_key.size_in_bytes(), 
-                                                    16, 16, -1) ]
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        session_key = cipher_rsa.decrypt(enc_session_key)
-        cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-        data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    
-    with open(filename, 'wb') as wobj:
-        wobj.write(data)     
-def Work_Decrypt(rootDir): 
+def Encrypt(filename,keydata):         
+    data = ''
+    with open(filename, 'rb') as f:
+        data = f.read()
+    with open(filename, 'wb') as out_file:
+        # 收件人秘钥 - 公钥
+        recipient_key = RSA.importKey(keydata)
+        session_key = get_random_bytes(16)
+        # Encrypt the session key with the public RSA key
+        cipher_rsa = PKCS1_OAEP.new(recipient_key)
+        out_file.write(cipher_rsa.encrypt(session_key))
+        # Encrypt the data with the AES session key
+        cipher_aes = AES.new(session_key, AES.MODE_EAX)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+        out_file.write(cipher_aes.nonce)
+        out_file.write(tag)
+        out_file.write(ciphertext)
+
+def Work_Encrypt(rootDir,key): 
     list_dirs = os.walk(rootDir) 
     for root, dirs, files in list_dirs: 
-        if True:   
+        if True:
             for f in files: 
                 filename = os.path.join(root, f)
-                print("Decrypt the file: " + filename)
-                Decrypt(filename)
+                print("Encrypt the file: " + filename)
+                Encrypt(filename,key)
