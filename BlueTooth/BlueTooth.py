36c3664f63de7142a00759ca2ca08ec5f6cabb85
Foenix <lzy1998@mail.ustc.edu.cn>
2018-11-17 18:17:36 +0800

    Query

--- BlueTooth/BlueTooth.py
+++ BlueTooth/BlueTooth.py
@@ -3,21 +3,25 @@ import time
 import pyautogui
 import subprocess
 import sys
+from dbus import Query
 sys.path.append("../locker")
 from config import config
 from trylock import locker
 
+
 while True:
+	time.sleep(3)
 	number = config['number']
 	os.system("sudo hciconfig hci0 up")
 	x = subprocess.check_output(['sudo','hcitool', 'cc', number], stderr=subprocess.STDOUT)
-	lzy = subprocess.check_output(['gnome-screensaver-command', '-q'])
-	if("Can" in str(x)):
-		os.system("gnome-screensaver-command -l")
-	elif ("The screensaver is inactive" in str(lzy)):
-		time.sleep(3)
+	# lzy = subprocess.check_output(['gnome-screensaver-command', '-q'])
+	lzy = Query()
+	if("Input/output" in str(x)):
+		# os.system("gnome-screensaver-command -l")
+		l = locker()
+		l.lock_workstation()
+	elif (lzy == 0):
 		continue
 	else:
 		l = locker()
 		l.unlock_workstation()
-		time.sleep(3)
--- BlueTooth/config.py
+++
@@ -1,6 +0,0 @@
-config = {
-    'lock': ['winleft', 'l'],
-    'unlockPasswd': 'renzhenghang',
-    'protectDir': ['/home/foenix/test'],
-    'number':'BC:3D:85:93:A9:E3',
-}
